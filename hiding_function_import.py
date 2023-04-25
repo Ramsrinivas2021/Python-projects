33bbf8fa62808dfe9167dde43a69837d2be1b1e5
Ramsrinivas2021 <93432007+Ramsrinivas2021@users.noreply.github.com>
2023-04-25 14:56:39 +0530

    Modified_module_name

--- hiding_function_import.py
+++ hiding_function_import.py
@@ -1,11 +1,11 @@
 # from practice_scopes_hiding_functions1 import *  # importing all the attributes in this file
-# import ractice_scopes_hiding_functions1
-from practice_scopes_hiding_functions1 import add  # using from import
+import practice_scopes_hiding_functions1
+# from practice_scopes_hiding_functions1 import add  # using from import
 sum1 = 50
-total = add()
+# total = add()
 # multiplication = mult()
 # print(multiplication)
-# total = practice_on_variable_scope._add()  # calling the function defined in add module
+total = practice_scopes_hiding_functions1.add()  # calling the function defined in add module
 # print(total)
 # sum1 = 50
 total = sum1 + total
